0b9a40308801cf682838b0563a17f9fb40c886a6
Guilherme Trano <GuilhermeTrajano.g27@gmail.com>
2026-06-04 13:09:19 -0300

    Criado primerias rotas

--- api.js
+++ api.js
@@ -2,4 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/tarefas', (req, res) => {
+  res.json([
+    { id: 1, title: 'Tarefa 1', completed: false }
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

c25956011cd8f19e1137dd85ddc9090e677d9eae
Guilherme Trano <GuilhermeTrajano.g27@gmail.com>
2026-08-09 15:04:55 -0300

    Instalado prisma

--- api.js
+++ api.js
@@ -10,10 +10,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/tarefas', (req, res) => {
-  res.json([
-    { id: 1, title: 'Tarefa 1', completed: false }
-  ]);
+  res.json(tasks);
+  res.status(200).json(tasks);
 });
+
+const tasks = [];
+app.post('/tarefas/enviar', (req, res) => {
+  tasks.push(req.body);
+  res.status(201).json(req.body);
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
